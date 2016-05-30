79d618524fb6f98db64e596f657b6c61e95cceea
Chris Gaona <thebeloved88@sbcglobal.net>
2016-05-30 12:27:21 -0700

    Adds functionality to create csv file for current day & add data from web scrape

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 var request = require('request');
+var json2csv = require('json2csv');
 
 var url = "http://www.shirts4mike.com/";
 
@@ -8,7 +9,6 @@ function scrape(req, res) {
   'use strict';
 
   if (req.url != '/favicon.ico') {
-    console.log(req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
     //The scraper should generate a folder called data if it doesn’t exist.
@@ -21,30 +21,53 @@ function scrape(req, res) {
         return false;
       }
     }
-    console.log(fileExists('data'));
 
     if (!fileExists('data')) {
       fs.mkdir('data');
     }
 
-    var $;
-    var object;
-
+    var productArray = [];
+    //The scraper should be able to visit the website http://shirts4mike.com and follow links to all t-shirts.
     //The scraper should get the price, title, url and image url from the product page and save it in the CSV.
     request(url + 'shirts.php/', function(err, res, html) {
-      $ = cheerio.load(html);
+      var $ = cheerio.load(html);
       $('ul.products li').each(function() {
         var productURL = $(this).find('a').attr('href');
-        console.log(url + productURL);
+        var today = new Date();
+
         request(url + productURL, function(err, res, html) {
           $ = cheerio.load(html);
-          object = {
-            price: $('.price').html(),
-            title: $('title').html(),
-            url: url + productURL,
-            imageURL: $('img').attr('src')
+          var productInfo = {
+            Title: $('title').html(),
+            Price: $('.price').html(),
+            ImageURL: $('img').attr('src'),
+            URL: url + productURL,
+            Time: today
           }
-          console.log(object);
+
+          productArray.push(productInfo);
+
+          var yyyy = today.getFullYear();
+          var mm = today.getMonth()+1;
+          var dd = today.getDate();
+          today = yyyy+'-'+mm+'-'+dd;
+
+          console.log(productInfo);
+          console.log(yyyy+'-'+mm+'-'+dd);
+
+          //The information from the site should be stored in a CSV file with today’s day e.g. 2016-01-29.csv.
+          //Use a third party npm package to create an CSV file. You should be able to explain why you chose that package.
+          //The column headers should be in in this order Title, Price, ImageURL, URL and Time. Time should be the current date time of when the scrape happened. If they aren’t in this order the can’t be entered into the database of the price comparison site.
+          var fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+
+          json2csv({data: productArray, fields: fields}, function(err, csv) {
+            if (err) console.log(err);
+
+            fs.writeFile('./data/' + today + '.csv', csv, function(err) {
+              if (err) throw err;
+              console.log('File Saved!');
+            });
+          });
         });
       });
     });
